87358a16ef8b5a4d9213b3bf7e2205da5218ec8d
George Mauer <gmauer@gmail.com>
2016-05-15 10:11:02 -0500

    ccan select tagsfor new filters

--- domain/actions/filters.js
+++ domain/actions/filters.js
@@ -1,14 +1,26 @@
 const {enqueAction} = require(`${baseDir}/ui/rendering`)
 
+const letters = /[A-Z]|\d/
+const parseSelected = (val, tags) =>  {
+	const tagChars = new Set(val.toUpperCase().split('').filter(c => letters.test(c)))
+	return t => tagChars.has(t.char)
+}
+
+const filtersFrom = (tags, change) => ({filters}) => ({
+	filters: Object.assign({}, filters, {newFilter: {
+		tags: tags.map(t => Object.assign({}, t, change(t)) )
+	}} )
+})
 
 const startAddNewFilter = messages => {
 	const tags = [ ...messages.reduce((s, m) => (m.tags.forEach(t => s.add(t)), s), new Set()) ]
-	enqueAction( ({filters}) => ({
-		filters: Object.assign({}, filters, {newFilter: {
-			tags: tags.map(t => Object.assign({}, t, {isSelected: false}) )
-		}} )
-	}) )
+	enqueAction(filtersFrom(tags, () => ({isSelected: false})) )
 }
+const configureNewFilter = (val, tags) => {
+	const isSelected = parseSelected(val, tags)
+	enqueAction(filtersFrom(tags, (t) => ({isSelected: isSelected(t)})) )
+}
+
 module.exports = {
-	startAddNewFilter,
+	startAddNewFilter, configureNewFilter,
 }
--- ui/filters/index.js
+++ ui/filters/index.js
@@ -1,27 +1,35 @@
 const { h } = require(`${baseDir}/ui/common/dom`)
-const { startAddNewFilter } = require(`${baseDir}/domain/actions/filters`)
+const classNames = require('classnames')
+const { startAddNewFilter, configureNewFilter } = require(`${baseDir}/domain/actions/filters`)
+const style = require(`${baseDir}/ui/common/style`)
 
-const expandedTag = ({color, name, char}) =>
+const expandedTag = ({color, name, char, isSelected}) =>
 	h('section.expanded.tag', {
-		style: {backgroundColor: color}
+		className: classNames({'selected': isSelected}),
+		style: {backgroundColor: color,},
 	}, [
 		h('.keycode', char),
 		h('.name', name)
 	])
 
 const filterSelector = ({tags}) =>
-	h('aside.new-filter', {}, [
+	h('aside.filter-builder', {}, [
 		h('h1', "Filter Selectors"),
 		h('ul',
 			tags.map(t =>
 				h('li', expandedTag(t))
 			)
 		),
-
+		h('.input-filter', [
+			h('input', {
+				onkeyup: e => configureNewFilter(e.target.value, tags)
+			}),
+		])
 	])
 
 module.exports = ({filters: { newFilter }, messages}) =>
 	h('section#filters', [
+		style('style'),
 		h('h1', "Filters"),
 		newFilter && filterSelector(newFilter),
 		h('button', {
